f4abebe3f56a516e1bdd0e6df2ff1adde72c8a22
karlen <m>
2015-12-07 01:10:17 +0400

    user java implemetation , refistration servlet

--- src/main/java/MainObjects/User.java
+++ src/main/java/MainObjects/User.java
@@ -1,80 +1,118 @@
 package MainObjects;
 
-import java.util.Objects;
+import java.io.IOException;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import Helpers.PasswordHash;
 
 /**
- * A class for serialization and deserialization of user.
- *
- * @auther Suren Martirosyan
- * @since 24.11.2015.
+ * 
+ * User main information class
+ * @author Karlen Mkrtchyan
  */
 public class User {
+	private String userPassword = null;
+	private String userLogin = null;
+	private String userName = null;
+	private Integer userID = null;
+	private String userKey = null;;
+	
+	public void setUserID(Integer userId) {
+		this.userID = userId;
+	}
+	
+	public Integer getUserID() {
+		return userID;
+	}
+	
+	public void setUserPassword(String userPassword,boolean isHashed){
+		if(isHashed){
+			this.userPassword = userPassword;
+		}else{
+			this.userPassword = PasswordHash.createHash(userPassword);
+		}
+	}
 
-    private Integer id;
-    private String userName;
-    private String mail;
-    private String password;
-    private String userKey;
+	public String getUserPassword() {
+		return userPassword;
+	}
 
-    public User (){}
+	public void setUserLogin(String userLogin) {
+		this.userLogin = userLogin;
+	}
 
-    public User(Integer id) {
-        this.id = Objects.requireNonNull(id);
-    }
+	public String getUserLogin() {
+		return userLogin;
+	}
 
-    public User (Integer id, String userName, String mail, String password, String userKey) {
-        this.id = id;
-        this.userName = userName;
-        this.mail = mail;
-        this.password = password;
-        this.userKey = userKey;
-    }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public boolean validateUser() {
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserKey() {
-        return userKey;
-    }
-
-    public void setUserKey(String userKey) {
-        this.userKey = userKey;
-    }
-
-    public String toJson() {
-        return null;
-        //TODO:
-    }
-    public void fromJson() {
-        //TODO:
-    }
+		return true;
+	}
+	
+	public String getHashedPassword(){	
+		return PasswordHash.createHash(this.userPassword);
+	}
+	
+	public void generateUserKey(){
+		if(this.userKey == null && this.userLogin != null && this.userPassword!=null){
+			userKey = PasswordHash.createHash(this.userLogin + this.userPassword);
+		}else{
+			System.out.print("can't generate user key: User.java");
+		}
+		
+	}
+	
+	public String getUserKey(){
+		return userKey;
+	}
+	
+	public void fromJson(String requestdata) throws JsonParseException, IOException{
+		ObjectMapper mapper = new ObjectMapper();
+        JsonFactory factory = mapper.getJsonFactory(); // since 2.1 use mapper.getFactory() instead
+        JsonParser jp = factory.createJsonParser(requestdata);
+        JsonNode node = mapper.readTree(jp);
+        if(node.get("userName") != null){
+        	 this.setUserName(node.get("userName").getTextValue());
+        }
+        if(node.get("email") != null){
+            this.setUserLogin(node.get("email").getTextValue());
+        }
+        if(node.get("password") != null){
+           this.setUserPassword(node.get("password").getTextValue(),false);	
+        }
+        this.generateUserKey();
+	}
+	
+	public String toJson() throws JsonParseException, IOException{
+		
+		 String responceString  = "{\"key\":\"" + this.getUserKey() + "\"";
+		 responceString  +=  ",\"userName\":\"" + this.getUserName() + "\"";
+         responceString += "}";
+		 return responceString;
+	}
+	
+	public boolean isUserVaild(){
+		
+		
+		
+		return true;
+	} 
+	
+	@Override
+	public String toString() {		
+		return "UserName- " + this.userName + " ,login = " + this.userLogin; 
+	}
 }
--- src/main/java/Servlet/RegistrationServlet.java
+++ src/main/java/Servlet/RegistrationServlet.java
@@ -1,31 +1,61 @@
 package Servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.map.ObjectMapper;
+import org.json.simple.JSONObject;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
+import ActionManagers.RegistrationManager;
+import Helpers.UnistoreException;
+import MainObjects.User;
 
 /**
- * Created by garik on 12/6/2015.
+ * class for actions coming for registration
+ * @author Karlen Mkrthcyan
+ *
  */
 public class RegistrationServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        String requestData = request.getParameter("data1");
-        ObjectMapper mapper = new ObjectMapper();
-        JsonFactory factory = mapper.getJsonFactory();
-        JsonParser jsonParser = factory.createJsonParser(requestData);
-        JsonNode requestJson = mapper.readTree(jsonParser);
+	  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.write("");
-    }
+	protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+	        String requestdata = request.getParameter("data");
+	        User newUser = null;
+	        try {
+				try {
+					//newUser = RegistrationManager.register(requestdata);
+				} catch (UnistoreException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally{
+				
+			}
+	        PrintWriter out = response.getWriter();
+	        if(newUser.getUserKey() != null){
+	            out.println("{");
+		        out.println("\"error\": \"user is not registred\",");
+		        out.println("}");
+		        out.close();
+	        }else{
+	            out.println("{");
+		        out.println("\"message\": \"user is registred\",");
+		        out.println("}");
+		        out.close();
+	        }
+	   
+	    }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        doPost(request, response);
-    }
+	    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+	        doPost(request, response);
+	    }
 }
